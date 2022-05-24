a127b3bbf4b0720222251527d3f71f64d403f3d8
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-05-24 22:08:11 +0200

    Show de facturas

---
+++ resources/views/livewire/facturas/show.blade.php
@@ -0,0 +1,29 @@
+<x-jet-dialog-modal wire:model="openShow">
+
+    <x-slot name="title">
+        <h1 class="text-2xl">Datos de la factura:</h1>
+    </x-slot>
+
+    <x-slot name="content">
+
+        <div class="bg-white rounded-md max-w-4xl mx-auto p-4 space-y-4 shadow-lg">
+        <h3 class=" mb-2 pt-3 font-semibold">Usuario al que va dirigida: <span class="font-thin">{{ $factura->user->name }}</span></p>
+        <h3 class="border-t mb-2 pt-3 font-semibold">Codigo: <span class="font-thin">{{ $factura->codigo }}</span></p>
+        <h3 class="border-t mb-2 pt-3 font-semibold">Suministros:
+            <span class="font-thin">
+                @foreach ($factura->suministros as $suministro)
+                    {{ $suministro->denominacion }}
+                @endforeach
+            </span></p>
+        <h3 class="border-t mb-2 pt-3 font-semibold">Precio total: <span class="font-thin">{{ $factura->suministros->sum('precio') }} €</span></p>
+    </div>
+
+    </x-slot>
+
+    <x-slot name="footer">
+        <button wire:click="$set('openShow', false)" class="p-2 pl-5 pr-5 bg-red-500 hover:bg-red-400 text-white rounded-full text-lg  focus:border-4 ring-red-500">
+            Volver
+        </button>
+    </x-slot>
+
+</x-jet-dialog-modal>
