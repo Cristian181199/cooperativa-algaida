50a9f4687f0806761d84898ea5b0e18c96e7e347
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-06-14 21:44:59 +0200

    Plantilla instalada

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+            <h2 class="text-xl font-semibold leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+            <x-button target="_blank" href="https://github.com/kamona-wd/kui-laravel-jetstream" variant="black"
+                class="items-center max-w-xs gap-2">
+                <x-icons.github class="w-6 h-6" aria-hidden="true" />
+                <span>Star on Github</span>
+            </x-button>
+        </div>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <x-welcome />
             </div>
         </div>
     </div>
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,45 +1,96 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+        <h2 class="text-xl font-semibold leading-tight">
+            {{ __('Profile Settings') }}
         </h2>
     </x-slot>
 
     <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+        <div class="grid gap-6 lg:grid-cols-4" x-data="{ selectedTab: 'profileSetteings' }">
+            <div class="flex flex-col items-start gap-2 lg:col-span-1">
+                @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                    <button 
+                        @click="selectedTab = 'profileSetteings'" 
+                        class="block w-full max-w-xs text-left px-4 py-2 rounded-md text-gray-600 dark:text-gray-400 transition-colors hover:text-gray-900 dark:hover:text-gray-200" 
+                        :class="{
+                            'bg-purple-500 text-white hover:text-white dark:text-white dark:hover:text-white': selectedTab == 'profileSetteings',
+                        }">
+                        {{ __('Profile Settings') }}
+                    </button>
+                @endif
 
-                <x-jet-section-border />
-            @endif
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                    <button 
+                        @click="selectedTab = 'updatePassword'" 
+                        class="block w-full max-w-xs text-left px-4 py-2 rounded-md text-gray-600 dark:text-gray-400 transition-colors hover:text-gray-900 dark:hover:text-gray-200" 
+                        :class="{
+                            'bg-purple-500 text-white hover:text-white dark:text-white dark:hover:text-white': selectedTab == 'updatePassword',
+                        }">
+                        {{ __('Update Password') }}
+                    </button>
+                @endif
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
-                </div>
+                @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                    <button 
+                        @click="selectedTab = 'twoFactorAuth'" 
+                        class="block w-full max-w-xs text-left px-4 py-2 rounded-md text-gray-600 dark:text-gray-400 transition-colors hover:text-gray-900 dark:hover:text-gray-200" 
+                        :class="{
+                            'bg-purple-500 text-white hover:text-white dark:text-white dark:hover:text-white': selectedTab == 'twoFactorAuth',
+                        }">
+                        {{ __('Two Factor Authentication') }}
+                    </button>
+                @endif
 
-                <x-jet-section-border />
-            @endif
+                <button 
+                    @click="selectedTab = 'browserSessions'" 
+                    class="block w-full max-w-xs text-left px-4 py-2 rounded-md text-gray-600 dark:text-gray-400 transition-colors hover:text-gray-900 dark:hover:text-gray-200" 
+                    :class="{
+                        'bg-purple-500 text-white hover:text-white dark:text-white dark:hover:text-white': selectedTab == 'browserSessions',
+                    }">
+                    {{ __('Browser Sessions') }}
+                </button>
 
-            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
-
-                <x-jet-section-border />
-            @endif
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.logout-other-browser-sessions-form')
+                @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                    <button 
+                            @click="selectedTab = 'deleteProfiles'" 
+                            class="block w-full max-w-xs text-left px-4 py-2 rounded-md text-gray-600 dark:text-gray-400 transition-colors hover:text-gray-900 dark:hover:text-gray-200" 
+                            :class="{
+                                'bg-purple-500 text-white hover:text-white dark:text-white dark:hover:text-white': selectedTab == 'deleteProfiles',
+                            }">
+                            {{ __('Delete Account') }}
+                        </button>
+                    @endif
             </div>
+            
+            <div class="lg:col-span-3">
+                @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                    <div x-show="selectedTab == 'profileSetteings'">
+                        @livewire('profile.update-profile-information-form')
+                    </div>
+                @endif
 
-            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-jet-section-border />
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                    <div x-show="selectedTab == 'updatePassword'" class="mt-10 sm:mt-0">
+                        @livewire('profile.update-password-form')
+                    </div>
+                @endif
 
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
+                @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                    <div x-show="selectedTab == 'twoFactorAuth'">
+                        @livewire('profile.two-factor-authentication-form')
+                    </div>
+                @endif
+
+                <div x-show="selectedTab == 'browserSessions'">
+                    @livewire('profile.logout-other-browser-sessions-form')
                 </div>
-            @endif
+
+                @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                    <div x-show="selectedTab == 'deleteProfiles'">
+                        @livewire('profile.delete-user-form')
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </x-app-layout>
